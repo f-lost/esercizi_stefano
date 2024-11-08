0ed37e3d188f2c7f73645ee38e93472f0a4045b1
f-lost <83578401+f-lost@users.noreply.github.com>
2024-11-08 11:08:38 +0100

    Update prove_numpy.py

--- corso_python/8.11/prove_numpy.py
+++ corso_python/8.11/prove_numpy.py
@@ -15,6 +15,33 @@ print("Media degli elementi:", arr.mean())  # Output: 3.0
 print("Valore massimo:", arr.max())  # Output: 5
 print("Indice del valore massimo:", arr.argmax())  # Output: 4"""
 
-arr3 = np.arange(6)
-reshaped_arr = arr3.reshape((2,3))
-print(reshaped_arr)
+# arr3 = np.arange(6)
+# reshaped_arr = arr3.reshape((2,3))
+# print(reshaped_arr)
+
+
+arr = np.array([1, 2, 3, 4, 5])
+
+# Indexing
+print(arr[0])  # Output: 1
+
+# Slicing
+print(arr[1:3])  # Output: [2 3]
+
+# Boolean Indexing
+print(arr[arr > 2])  # Output: [3 4 5]
+
+
+arr_2d = np.array([[1, 2, 3, 4],
+                   [5, 6, 7, 8],
+                   [9, 10, 11, 12]])
+# Slicing sulle righe
+print(arr_2d[1:3])  # Output: [[ 5  6  7  8]
+                   #          [ 9 10 11 12]]
+# Slicing sulle colonne
+print(arr_2d[:, 1:3])  # Output: [[ 2  3]
+                       #          [ 6  7]
+                       #          [10 11]]
+# Slicing misto
+print(arr_2d[1:3, 1:3])  # Output: [[ 6  7]
+                        #          [10 11]]
