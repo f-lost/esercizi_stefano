8f2de721b922a5de03d54394485469e63737591f
f-lost <83578401+f-lost@users.noreply.github.com>
2024-11-15 09:12:49 +0100

    Update pokedex.py

--- corso_python/14.11/pokedex.py
+++ corso_python/14.11/pokedex.py
@@ -1,11 +1,27 @@
 import json, requests, random
 
+def show_random_pokemon():
+    id = random.randint(1,1025)
 
-num = random.randint(1,1025)
+    url = f"https://pokeapi.co/api/v2/pokemon/{id}"
 
-url = f"https://pokeapi.co/api/v2/pokemon/{num}"
+    response = requests.get(url)
+    data = response.json()
 
-response = requests.get(url)
-data = response.json()
+    return (str(id), {"nome" : data["forms"][0],
+            "abilità" : data["abilities"],
+            "xp" : data["base_experience"],
+            "altezza" : data["height"],
+            "peso" : data["weight"]})
 
-print(data)
+
+pokedex = {}
+id, random_pokemon = show_random_pokemon()
+
+if random_pokemon in pokedex:
+
+    print("Pokemon già catturato.")
+
+else:
+
+    pokedex[id] = random_pokemon
