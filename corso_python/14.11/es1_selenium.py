6b3f7dfc0aabe2f053316babf3aecfdb81f9e356
f-lost <83578401+f-lost@users.noreply.github.com>
2024-11-18 09:24:59 +0100

    Update es1_selenium.py

--- corso_python/14.11/es1_selenium.py
+++ corso_python/14.11/es1_selenium.py
@@ -28,3 +28,16 @@ for i in range(6):
     i +=1
     riga[i] = tabella.find_element(By.XPATH, "/html/body/div[5]/div[1]/div[1]/div[3]/div/table/tbody/tr[{i}]" )
     print(riga(i))
+
+
+# driver = setup_driver()
+# driver.get("https://www.w3schools.com/html/html_tables.asp")
+# time.sleep(2)
+# button_privacy = driver.find_element(By.ID, "accept-choices")
+# button_privacy.click()
+# time.sleep(2)
+# for el in range(2,7):
+#     for num in range(1,4):
+#         tabella = driver.find_element(By.XPATH, f"/html/body/div[5]/div[1]/div[1]/div[3]/div/table/tbody/tr[{el}]/td[{num}]")
+#         print(tabella.text)
+# driver.quit()
