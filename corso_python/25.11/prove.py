2e050726b682ac59839ee459af782f05f97eed96
f-lost <83578401+f-lost@users.noreply.github.com>
2024-11-25 17:59:52 +0100

    Update prove.py

--- corso_python/25.11/prove.py
+++ corso_python/25.11/prove.py
@@ -3,6 +3,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 import numpy as np
 
 
@@ -13,15 +14,20 @@ y = data.target
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
 linear = LinearRegression()
 linear.fit(X_scaled, y)
-linear_predictions = np.round(linear.predict(X_scaled)).astype(int)
-linear_predictions = np.clip(linear_predictions, 0, len(np.unique(y)) - 1) 
-accuracy_linear = accuracy_score(y, linear_predictions)
-print(f"Accuratezza del modello lineare: {accuracy_linear:.2f}")
+y_pred = np.round(linear.predict(X_test)).astype(int)
 
-knn = KNeighborsClassifier(n_neighbors=3)
-knn.fit(X_scaled, y)
-knn_predictions = knn.predict(X_scaled)
-accuracy_knn = accuracy_score(y, knn_predictions)
-print(f"Accuratezza del modello knn: {accuracy_knn:.2f}")
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuratezza del modello: {accuracy:.2f}")
+
+
+label_map = {0: 'setosa', 1: 'versicolor', 2: 'virginica'}
+    
+y_test_labels = [label_map[x] for x in y_test]
+y_pred_labels = [label_map[x] for x in y_pred]
+
+for i in range(10):
+    print(f"Il fiore con dati: {X_test[i]} è un {y_test_labels[i]}, è stato predetto: {y_pred_labels[i]}")
